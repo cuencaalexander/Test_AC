10adb50aa47b1adaeb551f1f92ccc917f819628d
cuencaalexander <52808625+cuencaalexander@users.noreply.github.com>
2021-10-21 02:05:10 -0700

    Add files via upload

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,16 +19,135 @@ const CreativeForm = (props) => {
   const {
     vehicle,
   } = props;
-
+  
+  const [wheels, setWheels] = useState(vehicle.wheels || '');
   const [nickname, setNickname] = useState(vehicle.nickname || '');
+  const [mileage, setMileage] = useState(vehicle.mileage || '');
+  const [brand, setBrand] = useState(vehicle.brand || 'sedan');
+  const [engineStatus, setEngineStatus] = useState(vehicle.engine_status || 'works');
+  const [doors, setDoors] = useState(vehicle.doors || '');
+  const [seatStatus, setSeatStatus] = useState(vehicle.doors || '');
+
+
+
+  const brandSelect = () => {
+    
+    return (
+      <div className="field">
+        <label>Brand</label> 
+        <select id="vehicleBrand" value={brand} onChange={(e) => setBrand(e.target.value)} >
+          <option value="sedan"> Sedan </option>
+          <option value="coupe"> Coupe </option>
+          <option value="minivan"> Mini Van </option>
+          <option value="motorcycle"> Motorcycle </option>
+        </select>
+      </div>
+    )
+  }
+
+  const seatStatusSelect = () => {
+    if(brand == 'motorcycle'){
+        return (<div className="field">
+        <label>Seat Status</label>
+        <select value = {seatStatus} id="seatStatus" onChange={(e) => setSeatStatus(e.target.value)} >
+          <option value="works"> Works </option>
+          <option value="fixable"> Fixable </option>
+          <option value="Junk"> Junk </option>
+        </select>
+      </div>
+      )
+    } else {
+      return null
+    }
+  }
+
+  const wheelsSelect = () => {
+    if(brand == 'motorcycle') {
+      return (    
+      <div className="field">
+      <label>Wheels</label>
+      <select value = {wheels} id="wheels" onChange={(e) => setWheels(e.target.value)} >
+        <option value={0}> 0 </option>
+        <option value={1}> 1 </option>
+        <option value={2}> 2 </option>
+      </select>
+      </div>)
+    }
+    else {
+      return (
+        <div className="field">
+          <label>Wheels</label>
+          <select value = {wheels} id="wheels" onChange={(e) => setWheels(e.target.value)} >
+            <option value={0}> 0 </option>
+            <option value={1}> 1 </option>
+            <option value={2}> 2 </option>
+            <option value={3}> 3 </option>
+            <option value={4}> 4 </option>
+          </select>
+        </div> 
+      )
+    }
+  }
+
+  const doorsSelect = () => {
+    if(brand == 'motorcycle') {
+      return null;
+    }
+    else {
+      return (
+        <div className="field">
+          <label>Doors</label>
+          <select value = {doors} id="doors" onChange={(e) => setDoors(e.target.value)} >
+            <option value={0}> 0 </option>
+            <option value={1}> 1 </option>
+            <option value={2}> 2 </option>
+            <option value={3}> 3 </option>
+            <option value={4}> 4 </option>
+          </select>
+        </div> 
+      )
+    }
+  }
+
+  const engineStatusSelect = () => {
+    return (<div className="field">
+        <label>Engine Status</label>
+        <select value = {engineStatus} id="engineStatus" onChange={(e) => setEngineStatus(e.target.value)} >
+          <option value="works"> Works </option>
+          <option value="fixable"> Fixable </option>
+          <option value="Junk"> Junk </option>
+        </select>
+      </div>)
+  }
+
+  const mileageSelect = () => {
+    
+    return (
+    <div className="field">
+      <label>Mileage</label>
+      <input id="mileage" type="text" value={mileage} name='vehicle[mileage]'v onChange={(e) => setMileage(e.target.value)} />
+    </div>
+    )
+  }
+
+  const nickName = () => {
+    return (
+    <div className="field">
+    <label>Nickname</label>
+    <input id="nickname" type="text" value={nickname} name='vehicle[nickname]' onChange={(e) => setNickname(e.target.value)} />
+    <div>{nickname}</div>
+    </div>)
+  }
 
   return (
     <div>
-      <div className="field">
-        <label>Nickname</label>
-        <input id="nickname" type="text" value={nickname} name='vehicle[nickname]' onChange={(e) => setNickname(e.target.value)} />
-        <div>{nickname}</div>
-      </div>
+      {nickName()}
+      {brandSelect()}
+      {wheelsSelect()}
+      {engineStatusSelect()}
+      {mileageSelect()}
+      {doorsSelect()}
+      {seatStatusSelect()}
     </div>
   );
 };
